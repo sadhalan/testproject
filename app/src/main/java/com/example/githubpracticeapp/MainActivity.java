14261cbe2fe2e14047cedc12780ceeaf51570a6a
sadhalan <sadhaleanisha@gmail.com>
2024-01-18 21:12:56 -0500

    new comment on branch!

--- app/src/main/java/com/example/githubpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/githubpracticeapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //anisha sadhale just added a new comment and is testing merging of branches
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
