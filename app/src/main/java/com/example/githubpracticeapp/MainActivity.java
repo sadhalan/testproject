8db37e23f969bce106b3fd4a96e884edf73474ae
sadhalan <sadhaleanisha@gmail.com>
2024-01-18 21:09:01 -0500

    added comment

--- app/src/main/java/com/example/githubpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/githubpracticeapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //anisha added this comment here!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
