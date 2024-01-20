fefa810e91a746f834ddb0af0a8aa810f02a73e0
btalantbekova <bermet.talant@outlook.com>
2024-01-20 14:21:11 -0500

    added a new comment

--- app/src/main/java/com/example/githubpracticeapp/MainActivity.java
+++ app/src/main/java/com/example/githubpracticeapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     //anisha sadhale just added a new comment and is testing merging of branches
 
-    //bermet added this!!
+    //bermet added this!! and now this!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
